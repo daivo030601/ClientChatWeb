0d45c859552b8ebdf93d9a04edd4cbdb5bbb715f
O2F-IT\tvo9_mantu <daivobmt01@gmail.com>
2023-05-12 17:44:40 +0700

    UI web app

--- CleanChat.API/Program.cs
+++ CleanChat.API/Program.cs
@@ -6,11 +6,21 @@ using CleanChat.Infrastructure.context;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
 //Register configuration
 ConfigurationManager configuration = builder.Configuration;
 // Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("*")
+                          .AllowAnyHeader()
+                          .AllowAnyMethod();
+                      });
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -36,6 +46,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.MapControllers();
 
